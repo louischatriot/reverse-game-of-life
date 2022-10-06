8d80cff8233a06f4e07e28d82b3ad25c1e59639a
Louis Chatriot <louis.chatriot@gmail.com>
2022-10-07 00:02:54 +0200

    All predecessor for all kinds of cells generated

--- main.py
+++ main.py
@@ -1,7 +1,43 @@
+from data import nw, ne, sw, se, w, n, e, s, center
+
 
 def print_grid(input):
     for l in input:
         print(' '.join(['X' if i == 1 else '.' for i in l]))
+
+
+def get_neighbors(input, i, j):
+    N = len(input)
+    M = len(input[0])
+
+    neighbors = 0
+    for oi in [-1, 0, 1]:
+        for oj in [-1, 0, 1]:
+            if oi != 0 or oj != 0:
+                if i + oi >= 0 and i + oi < N:
+                    if j + oj >= 0 and j + oj < M:
+                        neighbors += input[i + oi][j + oj]
+
+    return neighbors
+
+
+# Get number of neighbors for one cell (can be center, border or corner cell)
+def get_cell_neighbor(cell, nw = False, ne = False, sw = False, se = False, w = False, n = False, e = False, s = False):
+    N = len(input)
+    M = len(input[0])
+
+    if nw:
+        if N != 2 or M != 2:
+            raise ValueError("Wrong cell size")
+        return get_neighbors(cell, 0, 0)
+
+
+def generate_all_predecessors(N, M):
+    res = [[]]
+    for i in range(0, N * M):
+        res = [[0] + c for c in res] + [[1] + c for c in res]
+
+    return [[c[i * M:(i + 1) * M] for i in range(0, N)] for c in res]
 
 
 def next(input):
@@ -12,23 +48,34 @@ def next(input):
 
     for i in range(0, N):
         for j in range(0, M):
-            neighbors = 0
-            for oi in [-1, 0, 1]:
-                for oj in [-1, 0, 1]:
-                    if oi != 0 or oj != 0:
-                        if i + oi >= 0 and i + oi < N:
-                            if j + oj >= 0 and j + oj < M:
-                                neighbors += input[i + oi][j + oj]
+            neighbors = get_neighbors(input, i, j)
 
             if neighbors == 3:
                 output[i][j] = 1
-            elif neighbors in [2, 3]:
+            elif neighbors == 2:
                 output[i][j] = input[i][j]
             else:
                 output[i][j] = 0
 
     return output
 
+
+
+
+# nw = {
+    # 0: [[0, 0, 0, 0], [1, 0, 0, 0]]
+# }
+
+
+def possible_predecesors():
+    pass
+
+
+def find_predecessor(goal):
+    N = len(input)
+    M = len(input[0])
+
+    possibilities = []
 
 
 
