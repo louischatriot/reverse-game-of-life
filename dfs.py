fead88efa7c2847b5c2bcb2cea5d4272d9ebfb4c
Louis Chatriot <louis.chatriot@gmail.com>
2022-10-17 20:30:59 +0200

    Added index

--- dfs.py
+++ dfs.py
@@ -15,6 +15,33 @@ t = Timer()
 
 
 
+def print_grid(input):
+    M = len(input[0])
+    print('-' * (2 * M - 1))
+    for l in input:
+        print(' '.join(['X' if i == 1 else '.' for i in l]))
+    print('-' * (2 * M - 1))
+
+
+def next(input):
+    N = len(input)
+    M = len(input[0])
+
+    output = [[0 for i in range(0, M)] for i in range(0, N)]
+
+    for i in range(0, N):
+        for j in range(0, M):
+            neighbors = get_neighbors(input, i, j)
+
+            if neighbors == 3:
+                output[i][j] = 1
+            elif neighbors == 2:
+                output[i][j] = input[i][j]
+            else:
+                output[i][j] = 0
+
+    return output
+
 
 def check_cols(l, c):
     N = len(l)
@@ -46,8 +73,6 @@ def hash(c):
     return n
 
 
-
-
 t.reset()
 
 p = generate_all_predecessors(3, 3)
@@ -63,55 +88,159 @@ for c in p:
 
 
 for i in range(0, np):
-    s = []
+    s = set()
     for j in range(0, np):
         if check_cols(cor[i], cor[j]):
-            s.append(j)
+            s.add(j)
     next_r.append(s)
 
-    s = []
+    s = set()
     for j in range(0, np):
         if check_rows(cor[i], cor[j]):
-            s.append(j)
+            s.add(j)
     next_b.append(s)
-
-print(len(next_b[234]))
-
-
-
-    # t = 0
-
-    # for cc in p:
-        # t+=1
-        # if check_cols(c, cc):
-            # next_r[hash(c)].append(hash(cc))
-
-        # if check_rows(c, cc):
-            # next_b[hash(c)].append(hash(cc))
-
-
-    # print(t)
-    # print(len(next_r[0]))
-    # 1/0
-
-# print(len(next_r[0]))
-
-# print(sum([len(i) for i in next_r]) / 512)
-
 
 
 t.time("Preparation done")
 
 
-# Generated using the following code and its variants
-# cells = generate_all_predecessors(3, 3)
-# nw = [[], []]
-# for c in cells:
-    # n = get_neighbors(c, 1, 1)
-    # if n == 3:
-        # nw[1].append(c)
-    # elif n == 2:
-        # nw[c[0][0]].append(c)
-    # else:
-        # nw[0].append(c)
+def get_neighbors(input, i, j):
+    N = len(input)
+    M = len(input[0])
+
+    neighbors = 0
+    for oi in [-1, 0, 1]:
+        for oj in [-1, 0, 1]:
+            if oi != 0 or oj != 0:
+                if i + oi >= 0 and i + oi < N:
+                    if j + oj >= 0 and j + oj < M:
+                        neighbors += input[i + oi][j + oj]
+
+    return neighbors
+
+
+def get_next(input, i, j):
+    neighbors = get_neighbors(input, i, j)
+
+    if neighbors == 3:
+        return 1
+    elif neighbors == 2:
+        return input[i][j]
+    else:
+        return 0
+
+
+center = [
+    {i for i in range(0, np) if get_next(cor[i], 1, 1) == 0},
+    {i for i in range(0, np) if get_next(cor[i], 1, 1) == 1}
+]
+
+
+nw = [i for i in range(0, np) if get_next(cor[i], 0, 0) == 0]
+nw = [i for i in nw if get_next(cor[i], 0, 1) == 0]
+nw = [i for i in nw if get_next(cor[i], 1, 0) == 0]
+
+n = [i for i in range(0, np) if get_next(cor[i], 0, 1) == 0]
+
+
+# n = [i for i in n if any(check_cols(cor[j], cor[i]) for j in nw)]
+
+# nw = [i for i in nw if any(check_cols(cor[i], cor[j]) for j in n)]
+
+# w = [i for i in range(0, np) if get_next(cor[i], 1, 0) == 0]
+
+# w = [i for i in w if any(check_rows(cor[j], cor[i]) for j in nw)]
+
+
+
+
+def clone_pos(pos):
+    N = len(pos)
+    M = len(pos[0])
+
+    clone = [[None] * M for i in range(0, N)]
+    for i in range(0, N):
+        for j in range(0, M):
+            clone[i][j] = pos[i][j].copy()
+
+    return clone
+
+
+def get_index(N, M):
+    idx = [(0, 0)]
+
+    i = 0
+    j = 0
+    while i < N-1 or j < M-1:
+        if j < M-1:
+            j += 1
+
+            for ii in range(0, i+1):
+                idx.append((ii, j))
+
+
+        if i < N-1:
+            i += 1
+
+            for jj in range(0, j+1):
+                idx.append((i, jj))
+
+
+
+
+    return idx
+
+
+
+idx = get_index(3, 5)
+
+print(len(idx))
+
+
+for i in idx:
+    print(i)
+
+
+1/0
+
+
+
+def find_predecessor(goal):
+    N = len(goal)
+    M = len(goal[0])
+
+    pos = [[None] * M for i in range(0, N)]
+
+    for i in range(0, N):
+        for j in range(0, M):
+            pos[i][j] = center[goal[i][j]]
+
+    pos[0][0] = pos[0][0].intersection(nw)
+
+    for j in range(1, M-1):
+        pos[0][j] = pos[0][j].intersection(n)
+
+
+
+
+
+
+
+
+
+
+t.reset()
+
+goal = [
+    [0, 0, 1, 1, 0, 0],
+    [1, 1, 1, 1, 0, 1],
+    [0, 0, 1, 1, 1, 1],
+    [1, 1, 1, 1, 0, 1]
+]
+
+res = find_predecessor(goal)
+
+t.time("Found one predecessor")
+
+
 
